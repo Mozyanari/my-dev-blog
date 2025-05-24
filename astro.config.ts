d694054b01515ae487fc84a9d9196a5232feb509
mozyanari <mozyanari@outlook.jp>
2025-05-24 22:09:58 +0900

    configを修正

--- astro.config.ts
+++ astro.config.ts
@@ -8,8 +8,8 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
 	// site: SITE.website,
-	site: "https://mozyanari.github.io/my-dev-blog",
-	output: "static",
+	site: "https://mozyanari.github.io/",
+	base: "/my-dev-blog",
 	integrations: [
 		sitemap({
 			filter: (page) => SITE.showArchives || !page.endsWith("/archives"),
