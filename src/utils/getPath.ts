df4f1dd61937e26a51b50c6754fcc58cd63052fe
mozyanari <mozyanari@outlook.jp>
2025-05-25 10:21:02 +0900

    getPathをする際にbasePathをgithubに対応するために修正

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -1,5 +1,6 @@
 import { BLOG_PATH } from "@/content.config";
 import { slugifyStr } from "./slugify";
+import { SITE } from "@/config";
 
 /**
  * Get full path of a blog post
@@ -9,28 +10,28 @@ import { slugifyStr } from "./slugify";
  * @returns blog post path
  */
 export function getPath(
-  id: string,
-  filePath: string | undefined,
-  includeBase = true
+	id: string,
+	filePath: string | undefined,
+	includeBase = true,
 ) {
-  const pathSegments = filePath
-    ?.replace(BLOG_PATH, "")
-    .split("/")
-    .filter(path => path !== "") // remove empty string in the segments ["", "other-path"] <- empty string will be removed
-    .filter(path => !path.startsWith("_")) // exclude directories start with underscore "_"
-    .slice(0, -1) // remove the last segment_ file name_ since it's unnecessary
-    .map(segment => slugifyStr(segment)); // slugify each segment path
+	const pathSegments = filePath
+		?.replace(BLOG_PATH, "")
+		.split("/")
+		.filter((path) => path !== "") // remove empty string in the segments ["", "other-path"] <- empty string will be removed
+		.filter((path) => !path.startsWith("_")) // exclude directories start with underscore "_"
+		.slice(0, -1) // remove the last segment_ file name_ since it's unnecessary
+		.map((segment) => slugifyStr(segment)); // slugify each segment path
 
-  const basePath = includeBase ? "/posts" : "";
+	const basePath = includeBase ? `${SITE.base}/posts` : "";
 
-  // Making sure `id` does not contain the directory
-  const blogId = id.split("/");
-  const slug = blogId.length > 0 ? blogId.slice(-1) : blogId;
+	// Making sure `id` does not contain the directory
+	const blogId = id.split("/");
+	const slug = blogId.length > 0 ? blogId.slice(-1) : blogId;
 
-  // If not inside the sub-dir, simply return the file path
-  if (!pathSegments || pathSegments.length < 1) {
-    return [basePath, slug].join("/");
-  }
+	// If not inside the sub-dir, simply return the file path
+	if (!pathSegments || pathSegments.length < 1) {
+		return [basePath, slug].join("/");
+	}
 
-  return [basePath, ...pathSegments, slug].join("/");
+	return [basePath, ...pathSegments, slug].join("/");
 }
